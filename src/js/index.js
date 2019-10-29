8668ea584e6594ab909fc47a2404337de82333c3
romanyakovlev <yakovlevroman8@gmail.com>
2019-10-29 13:43:35 +0700

    add folders for pug and scss

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,3 @@
-import './scss/base.scss';
+import '../scss/base.scss';
 
 console.log("hello, world");
--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,10 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 // webpack v4
 const path = require('path');
 module.exports = {
-  entry: { main: './src/index.js' },
+  entry: [
+    './src/js/index.js',
+    './src/scss/base.scss'
+  ],
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.js'
@@ -14,12 +17,14 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /node_modules/,
+        include: path.resolve(__dirname, 'src/js'),
         use: {
           loader: "babel-loader"
         }
       },
       {
         test: /\.scss$/,
+        include: path.resolve(__dirname, 'src/scss'),
         use: ExtractTextPlugin.extract({
           fallback: 'style-loader',
           use: ['css-loader', 'sass-loader']
@@ -27,14 +32,15 @@ module.exports = {
       },
       {
         test: /\.pug$/,
+        include: path.resolve(__dirname, 'src/pug'),
         loader: 'pug-loader'
       }
     ]
   },
   plugins: [
-    new ExtractTextPlugin('style.css'),
+    new ExtractTextPlugin('./css/base.css'),
     new HtmlWebpackPlugin({
-      template: './src/index.pug'
+      template: './src/pug/index.pug'
     }),
   ]
 };
